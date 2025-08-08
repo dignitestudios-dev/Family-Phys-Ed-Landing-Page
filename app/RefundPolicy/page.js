b5bbaddbe915664da810f7637ca54612050d2c55
Austin-seth <183656254+Austin-seth@users.noreply.github.com>
2025-08-08 00:07:45 -0700

    updata:RefundPolicy

--- app/RefundPolicy/page.js
+++ app/RefundPolicy/page.js
@@ -4,236 +4,62 @@
 export default function page() {
   return (
     <div className="w-full bg-[#1C1C1E] ">
-      <div className="text-white text-4xl text-center  font-bold p-12">Refund Policy for <span className="text-[#FDFC22]">Family Phys. Ed.</span></div>
-      {/* <div className="text-white leading-relaxed   px-12 pb-12 space-y-5">
-        <h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">1. Introduction</h2>
+      <div className="text-white text-4xl text-center  font-bold p-12">Refund and Cancellation Policy for <span className="text-[#FDFC22]">Family Phys. Ed.</span></div>
+       <div className="text-white leading-relaxed   px-12 pb-12 space-y-5"> 
+       
         <p className=" mb-6 px-8">
-          Welcome to Family Phys. Ed. (“the App”). The App is a marketplace that
-          connects users (“Clients”) with certified physical education teachers
-          (“Coaches”) to provide family training sessions and fun exercise
-          experiences. By accessing or using the App, you agree to comply with
-          these Terms and Conditions (“Terms”). If you do not agree, please do
-          not use the App
+         At Family Phys. Ed., we strive to provide a fair and transparent cancellation and refund policy 
+for both users and coaches. Please review the following guidelines: 
         </p>
         
-        <h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">2. User Eligibility</h2>
-<p className="mb-6 px-8">
-  You must be at least 18 years old to create an account and book services through the App. By
-  using the App, you represent that you meet this requirement.
-</p>
+        <h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">1. User Cancellations</h2>
 
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">3. Services Provided</h2>
-<p className="mb-6 px-8">
-  Family Phys. Ed. facilitates connections between Clients and Coaches for private training
-  sessions, family fitness events, and birthday parties. Family Phys. Ed. does not employ
-  Coaches but provides a platform for independent professionals to offer their services. We make
-  no representations or warranties regarding the qualifications or behavior of Coaches, and
-  Clients are encouraged to review Coach profiles and certifications before booking sessions.
-</p>
 
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">4. Account Registration</h2>
-<p className="mb-6 px-8">
-  To use the App, you must create an account and provide accurate, current, and complete
-  information as requested. You are responsible for maintaining the security of your account and
-  for any activity occurring under your account. You agree to notify us immediately of any
-  unauthorized access or use of your account.
-</p>
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">5. Booking and Payment</h2>
+
+
+
+
 <ul className="list-disc  mb-6 space-y-2 px-8">
   <li>
-    <strong>Booking:</strong> Clients may book sessions through the App by selecting available Coaches
-    and scheduling the session.
+  Users may cancel their session up to 24 hours before the scheduled start time to 
+  receive a full refund.
   </li>
   <li>
-    <strong>Payment:</strong> Payment for sessions is processed securely through the App via a third-party
-    payment processor, Stripe. Family Phys. Ed. may take a portion of the session, class, or
-    party price as its service fee before paying Coaches.
+  Cancellations made within 24 hours of the start time will not be eligible for a refund. 
+This policy is in place to protect our coaches from losing valuable opportunities due to 
+last-minute cancellations
   </li>
   <li>
-    <strong>Cancellation:</strong> Cancellation policies vary by Coach and will be displayed during the
-    booking process. Clients agree to adhere to these cancellation policies and any
-    applicable fees.
+  Partial refunds may be considered for cancellations made within 24 hours of the start 
+  time, but will be evaluated on a case-by-case basis by Family Phys. Ed. 
   </li>
 </ul>
 
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">6. Responsibilities of Users and Coaches</h2>
+<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">2. Coach Cancellations</h2>
 <ul className="list-disc  mb-6 space-y-2 px-8">
   <li>
-    <strong>Clients:</strong> Clients agree to provide a safe and appropriate environment for training
-    sessions.
+  Coaches may cancel a session up to 24 hours before the scheduled start time without 
+  penalty. 
   </li>
   <li>
-    <strong>Coaches:</strong> Coaches must have the necessary certifications and experience to provide
-    services. They are also responsible for maintaining the safety and well-being of Clients
-    during sessions.
+  If a coach cancels within 24 hours of the start time, the user will receive a full refund. 
   </li>
   <li>
-    <strong>Liability:</strong> Family Phys. Ed. is not responsible for any injuries, accidents, or disputes
-    between Clients and Coaches. Users acknowledge that participation in physical activities
-    carries inherent risks, and they assume full responsibility for their participation.
+  Coaches who cancel within 24 hours will be subject to warnings on their account. 
+Coaches may receive up to three warnings before further actions may be taken, 
+including potential suspension or removal from the platform. All warnings will be issued 
+and reviewed by Family Phys. Ed. on a case-by-case basis.
   </li>
 </ul>
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">7. Liability Disclaimer and Assumption of Risk</h2>
-<ul className="list-disc  mb-6 space-y-2 px-8">
-  <li>
-    <strong>Assumption of Risk:</strong> You acknowledge and agree that any physical activity, including
-    strength and cardiovascular training, carries an inherent risk of injury. These risks may
-    include, but are not limited to, minor injuries such as soreness, sprains, strains, and
-    bruises, or more serious injuries such as heart attack, stroke, paralysis, or death. By
-    using the services of Family Phys. Ed., you voluntarily assume all risks associated with
-    physical activity.
-  </li>
-  <li>
-    <strong>Waiver and Release of Liability:</strong> You agree to release, waive, and discharge Family
-    Phys. Ed., its officers, employees, agents, and contractors from any liability for injuries,
-    illnesses, or damages to property, including any personal or emotional injury, arising
-    from your participation in physical activities or use of the App.
-  </li>
-  <li>
-    <strong>Indemnification:</strong> You agree to indemnify and hold harmless Family Phys. Ed. and its
-    affiliates against any claims, costs, or expenses (including legal fees) arising from your
-    use of the App or participation in services.
-  </li>
-</ul>
+<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">3. Exceptional Circumstances</h2>
+<p className="mb-6 px-8">
+  We understand that life events can sometimes interfere with schedules. If a user or coach 
+needs to cancel within 24 hours, we encourage you to reach out via chat to explain the 
+situation. Please note that responses to messages sent through the chat may take 24-48 hours, 
+and we will make every effort to work with you on a solution.
+  </p>
 
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">8. Content and Conduct</h2>
-<ul className="list-disc  mb-6 space-y-2 px-8">
-  <li>
-    <strong>User Content:</strong> Users agree not to post or upload any content to the App that is
-    inappropriate, offensive, harmful, or violates any laws. This includes, but is not limited to,
-    defamatory, abusive, or discriminatory content.
-  </li>
-  <li>
-    <strong>Monitoring and Enforcement:</strong> Family Phys. Ed. reserves the right to remove content or
-    suspend accounts that violate these Terms. We may also suspend or terminate accounts
-    for abusive behavior, fraudulent activity, or other violations of these Terms.
-  </li>
-</ul>
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">9. Privacy Policy</h2>
-<p className="mb-6 px-8">
-  By using the App, you agree to the collection and use of your personal data as outlined in our
-  Privacy Policy. You understand that Family Phys. Ed. collects personal information as part of
-  the booking and payment process and shares that information with third-party service providers,
-  such as Stripe, for payment processing.
-</p>
-
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">10. Consent to Physical Contact</h2>
-<p className="mb-6 px-8">
-  You consent to the possibility of physical contact by Coaches during training sessions to ensure
-  proper form or technique. You understand that this contact will be appropriate and limited to the
-  scope of the training session.
-</p>
-
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">11. Use of Picture(s)/Film/Likeness</h2>
-<p className="mb-6 px-8">
-  You agree to allow Family Phys. Ed. to take pictures, film, and/or likenesses of you for
-  advertising and promotional purposes. If you do not wish for your likeness to be used for such
-  purposes, you agree to inform Family Phys. Ed. in writing.
-</p>
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">12. Force Majeure</h2>
-<p className="mb-6 px-8">
-  Family Phys. Ed. shall not be held liable for any delay or failure in performance due to causes
-  beyond its reasonable control, including but not limited to acts of God, war, pandemic,
-  government restrictions, or natural disasters.
-</p>
-
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">13. Intellectual Property</h2>
-<p className="mb-6 px-8">
-  All content provided through the App, including text, images, video, logos, and other materials,
-  is owned by Family Phys. Ed. or its licensors. Users may not reproduce, distribute, or otherwise
-  exploit any content without prior written consent from Family Phys. Ed. Users retain ownership
-  of the content they create within the App, but by submitting content, they grant Family Phys. Ed.
-  a license to use, modify, and distribute that content as necessary for the operation of the App.
-</p>
-
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">14. Governing Law</h2>
-<p className="mb-6 px-8">
-  These Terms and Conditions shall be governed by and construed in accordance with the laws
-  of the State of Pennsylvania. For users outside of Pennsylvania, by using the App, you agree
-  that any disputes arising out of or in connection with these Terms will be resolved in the courts
-  of Pennsylvania, and that the laws of Pennsylvania will govern any dispute, except where
-  local laws of the user’s jurisdiction provide otherwise.
-</p>
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">15. Dispute Resolution/Arbitration</h2>
-<p className="mb-6 px-8">
-  Any dispute or claim arising out of or relating to these Terms shall first be attempted to be
-  resolved through mediation. If the dispute is not resolved within 30 days, it will then be resolved
-  through binding arbitration, as outlined in this section. The arbitration will be conducted by the
-  American Arbitration Association (AAA) under its rules and procedures. The decision of the
-  arbitrator shall be final and binding.
-</p>
-
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">16. Termination of Accounts</h2>
-<p className="mb-6 px-8">
-  Family Phys. Ed. reserves the right to suspend or terminate accounts that violate these Terms,
-  including, but not limited to, illegal activity, abusive behavior, or failure to comply with the
-  responsibilities outlined in these Terms.
-</p>
-
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">17. Changes to Terms</h2>
-<p className="mb-6 px-8">
-  Family Phys. Ed. may update or modify these Terms at any time. We will notify users of any
-  significant changes via email or in-app notifications. Continued use of the App after changes are
-  made constitutes acceptance of the revised Terms.
-</p>
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">18. Parental Consent and Responsibility</h2>
-<p className="mb-6 px-8">
-  By booking sessions through the App, parents/guardians are responsible for providing the
-  necessary information on behalf of the children participating in physical education sessions.
-  Parents/guardians must ensure that their children’s health information, activity levels, and any
-  other relevant details are accurate and up to date. Parents/guardians also agree to supervise
-  their children during all sessions, ensuring that the environment remains safe.
-</p>
-
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">19. Assumption of Risk for Children</h2>
-<p className="mb-6 px-8">
-  Parents/guardians acknowledge that physical activity for children also carries inherent risks,
-  such as the risk of injury. By allowing their children to participate in sessions, parents/guardians
-  accept all risks on behalf of the child. This includes, but is not limited to, minor injuries
-  (soreness, sprains, strains) and more serious risks (such as heart attack, stroke, or other
-  accidents). Family Phys. Ed. is not responsible for any injuries that occur during sessions, and
-  parents/guardians assume full responsibility for their child’s participation.
-</p>
-
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">20. Coaches' Responsibilities When Working with Children</h2>
-<p className="mb-6 px-8">
-  Coaches who provide services to children must hold the necessary certifications and have
-  relevant experience to ensure a safe and appropriate training environment. Coaches must
-  adhere to all safety guidelines provided by Family Phys. Ed. and maintain professional conduct
-  when interacting with children. Parents/guardians should feel free to inquire about coaches'
-  certifications before booking sessions.
-</p>
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">21. Parental Supervision</h2>
-<p className="mb-6 px-8">
-  Parents/guardians are required to supervise their children during any private sessions or
-  family fitness events unless the session is held in a supervised facility or organized group
-  class. Family Phys. Ed. does not assume responsibility for the child’s actions or safety outside
-  of the agreed-upon training session.
-</p>
-
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">22. Child Safety Guidelines</h2>
-<p className="mb-6 px-8">
-  All activities facilitated by Family Phys. Ed. are conducted with the highest regard for child
-  safety. Parents/guardians agree to disclose any relevant health information that may affect the
-  child's participation in physical activities. Family Phys. Ed. and Coaches are committed to
-  providing a safe environment during all sessions, including adhering to safety protocols for
-  exercises and physical activities.
-</p>
-
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">23. Use of Picture(s)/Film/Likeness for Children</h2>
-<p className="mb-6 px-8">
-  If photos, films, or likenesses of children are taken during a session, Family Phys. Ed. requires
-  written consent from the child’s parent or guardian before using the images for advertising or
-  promotional purposes. If a parent or guardian does not wish for their child’s likeness to be used,
-  they must inform Family Phys. Ed. in writing prior to or at the time of the session.
-</p>
-<h2 className="text-3xl font-bold mt-10 mb-2 text-[#FDFC22]">24. Contact Information</h2>
-<p className="mb-6 px-8">
-  For questions regarding these Terms, please contact us at:<br />
-  <span className="block">Email: <a href="mailto:info@familyphysed.com" className="text-blue-600 underline">info@familyphysed.com</a></span>
-  <span className="block">Phone: 1-484-849-802</span>
-</p>
-    </div> */}
+    </div>
     </div>
   );
 }
